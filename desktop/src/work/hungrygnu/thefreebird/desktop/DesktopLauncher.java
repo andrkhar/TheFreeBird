9b83772f478d1da26bbcf9e7aafff860d2a30ae2
Hungry <andrkhar@gmail.com>
2016-02-11 23:33:24 +0300

    The project init.

---
+++ desktop/src/work/hungrygnu/thefreebird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package work.hungrygnu.thefreebird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import work.hungrygnu.thefreebird.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
